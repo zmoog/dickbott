df184cd5cb1fb4659b7d57d0e92a6bc28f6f2a25
Mario Gualtieri <mario.gualtieri2012@gmail.com>
2019-01-17 11:49:43 +0100

    Add ec2 terminate support

--- scripts/aws/ec2/IEC2Service.ts
+++ scripts/aws/ec2/IEC2Service.ts
@@ -9,4 +9,5 @@ export interface IEC2Service {
     describeImages(params: EC2.Types.DescribeImagesRequest): Promise<EC2.Types.DescribeImagesResult>;
     copyImage(params: EC2.Types.CopyImageRequest): Promise<EC2.Types.CopyImageResult>;
     createTags(params: EC2.Types.CreateTagsRequest): Promise<{}>;
+    terminateIstances(params: EC2.Types.TerminateInstancesRequest): Promise<EC2.Types.TerminateInstancesResult>;
 }
